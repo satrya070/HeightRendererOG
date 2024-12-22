2df8f72dab5332deb16e8ba35e49f4192246a2c7
satrya070 <satrya@spotr.ai>
2024-12-22 23:22:58 +0100

    add standard fucntions and fix bugs

--- main.cpp
+++ main.cpp
@@ -6,6 +6,30 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #include <vector>
+#include "Shader.h"
+#include "camera.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+//void key_callback(GLFWwindow* window, int key, int scancode, int action, int modifiers);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
+
+const unsigned int WIDTH = 800;
+const unsigned int HEIGHT = 600;
+
+Camera camera(
+	glm::vec3(67.f, 627.f, 169.f),
+	glm::vec3(0.0f, 1.0f, 0.0f),
+	-128.1f,
+	-42.4f
+);
+float lastX = WIDTH / 2.0f;
+float lastY = HEIGHT / 2.0f;
+bool firstMouse = true;
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
 int main()
 {
@@ -21,7 +45,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Heightmap Renderer", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL Heightmap Renderer", nullptr, nullptr);
 	if (!window)
 	{
 		std::cerr << "Failed to create GLFW window" << std::endl;
@@ -30,6 +54,11 @@ int main()
 	}
 
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	//glfwSetKeyCall(window, key_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	// load openGL extension functions with GLAD
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -37,6 +66,11 @@ int main()
 		std::cerr << "Failed to initialize GLAD!" << std::endl;
 		return -1;
 	}
+
+	glEnable(GL_DEPTH_TEST);
+
+	// 
+	Shader HeightShader("vertex_shader.txt", "fragment_shader.txt");
 
 	// load map
 	int cols, rows, nChannels;
@@ -57,13 +91,15 @@ int main()
 				unsigned char height = *pixelOffset;
 
 				// sets origin to middel of (rows, cols)
-				vertices.push_back(-rows / 2.0f + r);
-				vertices.push_back(height * yScale - yShift);
-				vertices.push_back(-cols / 2.0 + c);
+				vertices.push_back(-rows / 2.0f + (r * rows / (float)rows)); // X
+				vertices.push_back((int)height * yScale - yShift); // Y
+				vertices.push_back(-cols / 2.0 + (c * cols / (float)cols)); // Z
 			}
 		}
 	}
 	stbi_image_free(data);
+
+	std::cout << "loaded: " << vertices.size() / 3 << " vertices" << std::endl;
 
 	// EBO indices
 	std::vector<unsigned int> indices;
@@ -75,7 +111,7 @@ int main()
 			for (unsigned int k = 0; k < 2; k++)
 			{
 				// picks the top c, then the one below
-				indices.push_back(c + rows * (r + k));
+				indices.push_back(c + cols * (r + k));
 			}
 		}
 	}
@@ -85,7 +121,7 @@ int main()
 	GLuint terrainVAO, terrainVBO, terrainEBO;
 	glGenVertexArrays(1, &terrainVAO);
 	glBindVertexArray(terrainVAO);
-	glVertexAttribPointer(0, 1, GL_FLOAT, GL_FALSE, sizeof(float), &vertices[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glBindVertexArray(0);
 
 	glGenBuffers(1, &terrainVBO);
@@ -97,22 +133,37 @@ int main()
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
 
 	const unsigned int N_STRIPS = rows - 1;
-	const unsigned int N_VERTS_PER_STRIP = rows * 2;
+	const unsigned int N_VERTS_PER_STRIP = cols * 2;
+
+	std::cout << "number of strips: " << N_STRIPS << std::endl;
+	std::cout << "number of triangles: " << N_VERTS_PER_STRIP << std::endl;
 
 	// main loop
 	while (!glfwWindowShouldClose(window))
 	{
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		processInput(window);
+
 		// clear buffers
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// swap
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+		// activate shader before drawing and uniforms
+		HeightShader.use();
+
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), ((float)WIDTH / (float)HEIGHT), 0.1f, 100000.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		glm::mat4 model = glm::mat4(1.0f);
+		HeightShader.setMat4("projection", projection);
+		HeightShader.setMat4("view", view);
+		HeightShader.setMat4("model", model);
 
 		// render cube
 		glBindVertexArray(terrainVAO);
-		for (unsigned int strip = 0; strip << N_STRIPS; strip++)
+		for (int strip = 0; strip < N_STRIPS; strip++)
 		{
 			glDrawElements(
 				GL_TRIANGLE_STRIP,
@@ -121,10 +172,80 @@ int main()
 				(void*)(sizeof(unsigned int) * N_VERTS_PER_STRIP * strip)
 			);
 		}
+
+		// swap
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
+
+	// delete all used sources
+	glDeleteVertexArrays(1, &terrainVAO);
+	glDeleteBuffers(1, &terrainVBO);
+	glDeleteBuffers(1, &terrainEBO);
 
 	glfwTerminate();
 
 	//std::cout << "pointer: " << data;
 	return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+/*void key_callback(GLFWwindow* window, int key, int scancode, int action, int modifiers)
+{
+	if (action == GLFW_PRESS)
+	{
+		switch (key)
+		{
+		case GLFW_KEY_SPACE:
+			useWireframe = 1 - useWireframe;
+			break;
+		case GLFW_KEY_G:
+			displayGrayscale = 1 - displayGrayscale;
+			break;
+		default:
+			break;
+		}
+	}
+}*/
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
 }
